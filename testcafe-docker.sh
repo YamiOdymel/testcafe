137d9580d019c679de916595c05fe9fa8f015f44
Yami Odymel <yamiodymel@gmail.com>
2017-03-21 23:14:50 +0800

    Update testcafe-docker.sh

--- testcafe-docker.sh
+++ testcafe-docker.sh
@@ -6,4 +6,4 @@ dbus-daemon --session --fork
 Xvfb :1 -screen 0 "${XVFB_SCREEN_WIDTH}x${XVFB_SCREEN_HEIGHT}x24" >/dev/null 2>&1 &
 export DISPLAY=:1.0
 fluxbox >/dev/null 2>&1 &
-node ./bin/testcafe.js "$@"
+node ./node_modules/testcafe/bin/testcafe.js "$@"
